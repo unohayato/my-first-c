4d89c15b65fddd8a6feb892fbd0168a447dba0cc
unohayato <mamidori1p1p@gmail.com>
2022-09-01 21:29:55 +0900

    calc

--- hello.c
+++ hello.c
@@ -2,8 +2,7 @@
 int main(void) {
   printf("Hello World!\n");
   printf("Hello World!\n");
-  printf("%d", 100);
-  printf("円");
+  printf("%d円\n", 100*100);
   printf("%d+%d=%d", 100, 200, 300);
   return 0;
 }

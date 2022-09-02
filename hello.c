31f023b5151ec07bb00225c19a590b0c1dbc8751
unohayato <mamidori1p1p@gmail.com>
2022-09-02 20:03:10 +0900

    キャスト変換

--- hello.c
+++ hello.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main(void) {
   int value;
-  value = 100;
-  printf("Hello World!\n");
-  printf("Hello World!\n");
-  printf("%d円\n", value*value);
-  printf("%d+%d=%d", value, value*2, value*3);
+  printf("%d", (int) (3.14*3*3));
   return 0;
 }

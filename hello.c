2f925deacfaef12c58ba5e7a61c5ee346f486987
unohayato <mamidori1p1p@gmail.com>
2022-09-04 00:11:45 +0900

    while

--- hello.c
+++ hello.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main(void) {
-  int i;
-  for (i = 0; i <= 10; i++) {
-    printf("%d", i);
+  int year = 1;
+  double money = 1000000;
+  while (money < 100000000) {
+    year += 1;
+    money *= 1.05;
   }
+
+  printf("%d年目: %f円となり一億円を超えます", year, money);
+
   return 0;  
 }
 

fadba44262a4e993b311df009cfddfce49059fc8
unohayato <mamidori1p1p@gmail.com>
2022-09-05 21:21:16 +0900

    関数（複数引数）

--- hello.c
+++ hello.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 int calc(int price);
+int fruits(int orange, int apple);
 int main(void) {
 
-  calc(300);
+  fruits(2, 2);
   return 0;
 }
 
 int calc (int price) {
   printf("%f", price * 1.1);
+  return 0;
+}
+
+int fruits(int orange, int apple) {
+  printf("%d", (orange * 200) + (apple * 2));
   return 0;
 }
 

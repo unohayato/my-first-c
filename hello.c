2c88044a496651df95a6a3e8ba8b2dbff5fa66fe
unohayato <mamidori1p1p@gmail.com>
2022-09-06 20:23:49 +0900

    配列　初期化

--- hello.c
+++ hello.c
@@ -5,9 +5,13 @@ int apple = 10;
 
 int main(void) {
 
-  int array[10];
-  array[5] = 15;
-  printf("%d", array[5]);
+  int array [5] = {0, 1, 2, 3, 4};
+
+  printf("%d", array[0]);
+  printf("%d", array[1]);
+  printf("%d", array[2]);
+  printf("%d", array[3]);
+  printf("%d", array[4]);
 
   
   return 0;

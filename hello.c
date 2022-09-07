bdda9744eb04dda409e72c2f336fb68aa16a5983
unohayato <mamidori1p1p@gmail.com>
2022-09-07 22:00:24 +0900

    sizeof

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int main(void) {
 
   int array[] = {11, 12, 13, 14};
   int i;
-  for (i = 0; i < 5; i++) {
+  for (i = 0; i < sizeof(array) / sizeof(array[0]); i++) {
     printf("%d番目は%d\n", i, array[i]);
 
   }

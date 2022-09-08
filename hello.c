d5420c0d1cb15d56ee58f4a3745e15741ff19a96
unohayato <mamidori1p1p@gmail.com>
2022-09-09 08:51:32 +0900

    strlen

--- hello.c
+++ hello.c
@@ -3,13 +3,11 @@
 
 int main(void) {
 
-  char str [15];
-  char str1 [] = "Star";
-  char str2 [] = "Wars";
-  int s = 4;
-  sprintf(str, "%s%s%d", str1, str2, s);
-  printf("%s", str);
-
+  char str[256];
+  scanf("%s", str);
+  int i = strlen(str);
+  printf("%d", i);
+  
 
 
   

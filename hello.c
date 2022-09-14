f10397a09859902879cec7bfb0cd8074ca0aa00a
unohayato <mamidori1p1p@gmail.com>
2022-09-14 10:43:15 +0900

    構造体　引数

--- hello.c
+++ hello.c
@@ -7,18 +7,19 @@ struct Fruits {
   int orange;
 };
 
+void price(struct Fruits store1);
 
 int main (void) {
-
   struct Fruits store1 = {100, 200, 300};
+  price(store1);
+  return 0;
+};
+
+void price (struct Fruits store1) {
   printf("%d\n", store1.apple);
   printf("%d\n", store1.banana);
   printf("%d\n", store1.orange);
-
-
-  return 0;
-}
-
+};
 
 
   
